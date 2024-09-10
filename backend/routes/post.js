abd18f1c75d4cb1fc14e47df895bd1806ee698fa
Youssef Ababou <3b.youssef@mail.com>
2024-09-10 16:28:02 +0100

    Add Post Route and Controller

---
+++ backend/routes/post.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const { Post } = require("../models/Post");
+const userArr = require("../helper/userArr");
+const router = express.Router();
+
+/**
+ * @desc Get All Posts
+ * @route /api/post
+ * @method GET
+ * @access public
+ */
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().populate("user", userArr);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+});
+
+/**
+ * @desc Get Post By His Id
+ * @route /api/post/:id
+ * @method GET
+ * @access public
+ */
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate("user", userArr);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+});
+/**
+ * @desc Create a new post
+ * @route /api/post
+ * @method POST
+ * @access public
+ */
+
+router.post("/", async (req, res) => {
+  try {
+    const post = new Post({
+      user: req.body.user,
+      title: req.body.title,
+      image: req.body.image,
+    });
+    const result = await post.save();
+    if (result) {
+      res.status(201).json(post);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+});
+
+/**
+ * @desc Edit Post From His Id
+ * @route /api/Post/:id
+ * @method PUT
+ * @access public
+ */
+
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          user: req.body.user,
+          title: req.body.title,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+
+    if (post) {
+      res.status(200).json({ message: "Post updated successfully" });
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went Wrong", error });
+  }
+});
+
+/**
+ * @desc Delete Post From His Id
+ * @route /api/post/:id
+ * @method DELETE
+ * @access public
+ */
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post) {
+      await Post.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "Post Has Been Deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Post Not Found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong", error });
+  }
+});
+module.exports = router;
