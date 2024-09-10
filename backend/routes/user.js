598199fe520a924531b94e7fa4af767cbdb98e5a
Youssef Ababou <3b.youssef@mail.com>
2024-09-10 12:50:03 +0100

    Find User By His Id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -16,4 +16,23 @@ router.get("/allusers", async (req, res) => {
     res.status(500).json({ message: "Something wen Wrong", error });
   }
 });
+
+/**
+ * @path /api/user/getUserById
+ * @method GET
+ * @description Get User From His Id
+ */
+
+router.get("/getUserById/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Something wen wrong", error });
+  }
+});
 module.exports = router;
