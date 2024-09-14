dd72963e3aa9c1292f092cd860f2427e7a861643
Youssef Ababou <3b.youssef@mail.com>
2024-09-14 13:41:17 +0100

    ui Menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* Auth Routes  */}
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
+
+          {/* Routes App */}
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
