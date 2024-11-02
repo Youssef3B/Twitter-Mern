bfce7c37798475572474a7714694d4c464064aae
Youssef Ababou <3b.youssef@gmail.com>
2024-11-02 04:52:02 +0100

    Filter Page Home

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,13 +1,13 @@
 import Post from "./Post";
 
-function Posts() {
+function Posts({posts}) {
   return (
     <div className="py-4 ">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.length<= 0 ? <h1 className="text-2xl text-center my-10 font-semibold">No posts available. Try following users to see their posts.</h1> :posts.map((post) => (
+        <Post key={post._id} post={post} />
+        )) 
+      }
+  
     </div>
   );
 }
