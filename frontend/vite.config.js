b7fc94508adcdae9b78efaaad35a5d26b23f1185
Youssef Ababou <3b.youssef@gmail.com>
2024-11-13 20:58:02 +0100

    add some link in app.js 15

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   "eslint.options": { setting: true },
+  build: {
+    outDir: "dist", // This should be 'dist' by default
+  },
 });
