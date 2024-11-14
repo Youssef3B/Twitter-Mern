680e7fcc38b90dd829a112432017c1293425a96d
Youssef Ababou <3b.youssef@gmail.com>
2024-11-14 10:26:05 +0100

    v55

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /**
- * @path /api/user/allusers
+ * @path /api/user/allusers test
  * @method GET
  * @description Get All Users
  */
